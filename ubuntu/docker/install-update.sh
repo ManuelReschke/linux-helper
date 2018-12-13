6218b8970a53d1f77c80c7425d352120eef88280
ManuelReschke <manuel.reschke@invia.de>
2018-12-13 16:24:16 +0100

    * set default compose version to 1.23.2

--- ubuntu/docker/install-update.sh
+++ ubuntu/docker/install-update.sh
@@ -8,7 +8,7 @@
 DOCKER_CHANNEL="stable"
 
 # latest 1.22.0 -> https://docs.docker.com/release-notes/docker-compose/
-DOCKER_COMPOSE_VERSION="1.22.0"
+DOCKER_COMPOSE_VERSION="1.23.2"
 
 #########################
 ## Remove old Versions ##
